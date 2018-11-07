418c80dfc09e44961e24da3835a9d05b0780c4a0
Александр Трофимов <trofimoff@live.ru>
2018-11-07 08:06:34 +0300

    --

--- hls-hash/command-function.php
+++ hls-hash/command-function.php
@@ -1,18 +1,4 @@
 <?php
-
-function _command($cmd){
-  $output = null;
-  
-  exec($cmd, $result, $error);
-  
-  if (!empty($result)) {
-      $result = implode("\n", (array) $result);
-      $output .= $result;
-  }
-  
-  return trim((string) $output);
-}
-
 function _command_curl($input){
   $result = file_get_contents($input);
   $result = trim($result);
