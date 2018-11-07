df0afe6f9f88c5e6ad2f125db5c4b1dc73db2b5a
Александр Трофимов <trofimoff@live.ru>
2018-11-07 22:17:37 +0300

    .input-source wrapper

--- hls-hash/app.php
+++ hls-hash/app.php
@@ -25,11 +25,12 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" language="javascript" type="text/javascript"></script>
   <script language="javascript" type="text/javascript">
   $(document).ready(function() {
-    var input  = $('.input-source').first().find('code').html(),
+    var input  = $('.input-source').find('code').html(),
         source = 'nplayer-' + input;
     
     //document.location.href = source;
-    $('.play-action').first().attr({ href: source });
+    $('a.play-action').attr({ href: source });
+    document.location.href = source;
   });
   </script>
 </head>
--- hqporner/curl-hqporner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -s "$1" > temp.html
-result=$(cat temp.html | grep -oP '\/{2}[^\"]+(1080|720)'\.mp4 | head -n 2)
-rm -f temp.html
-echo $result
