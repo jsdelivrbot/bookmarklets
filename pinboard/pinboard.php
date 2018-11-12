a9b90df99b91fd1f5cdd2f71365fef66ef911f75
Александр Трофимов <trofimoff@live.ru>
2018-11-12 05:02:02 +0300

    --

--- pinboard/pinboard.php
+++ pinboard/pinboard.php
@@ -8,25 +8,31 @@ $version = (string) phpversion();
 function app($input){
   global $client,
          $version;
-  $input = explode(":", $input, 2);
-  $link= 'https://pinboard.in/add?replace=yes&shared=no&tags=.adult%20video%20vk&url=https%3A%2F%2Fm.vk.com%2Fvideo-148989495_456245395&title=Blue%20Pill%20Men%20%2F%20Голубые%20Таблетки%20Для%20Мужчин%20(Aaliyah%20Hadid%2C%20Victoria%20Valencia%2C%20Stacie)&description=https%3A%2F%2Fpp.userapi.com%2Fc850636%2Fv850636637%2F1ed13%2FuyjS7X60oqA.jpg';
-  $action = $input[0];
-  $input  = $input[1];
+  //$input = explode(":", $input, 2);
+  $action = $_GET['action'];
+  $input  = $input;
   switch($action) {
     case 'add':
       parse_str($input, $i);
       $input = array();
       foreach($i as $prop => $val){
         $prop = trim(urldecode($prop));
-        $val  = trim(urldecode($val)));
+        $val  = trim(urldecode($val));
         $input[$prop] = $val;
       } unset($i);
       $input = (object) $input;
+      break;
+      
+    case 'list':
+      $input = (object) parse_url($_SERVER['QUERY_STRING']);
+      var_dump($input); exit();
       break;
   }
 
   // $curl = $client->request('GET', $input);  
   // $out = (string) $cutl->getBody();
   return strval($out);
+};
+return app($_REQUEST);
 ?>
 
